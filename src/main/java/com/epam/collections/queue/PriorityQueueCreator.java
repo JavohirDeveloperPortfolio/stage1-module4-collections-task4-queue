2c1907e6e74a031ce7182d640a55388ff674ff0a
JavohirDeveloperPortfolio <haqberdiyev99@bk.ru>
2022-12-27 21:43:03 +0500

    some issue

--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,33 @@
 package com.epam.collections.queue;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> queue = new PriorityQueue<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o2.compareTo(o1) <= 0){
+                    return -1;
+                }else {
+                    return 1;
+                }
+            }
+        });
+
+        for (String s : firstList) {
+            if (queue.contains(s)){
+                queue.add(s);
+            }
+        }
+        for (String s : secondList) {
+            if (queue.contains(s)){
+                queue.add(s);
+            }
+        }
+
+        return queue;
     }
 }
